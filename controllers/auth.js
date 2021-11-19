fb67f6af658c3e6e015037b2e5e7fa3ed85a2a6a
Jeremy Ledet <LedetJeremyJ@JohnDeere.com>
2021-11-19 10:29:56 -0600

    changes to JWT to see if fixes

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,7 +4,7 @@ const User = require('../models').User;
 const constants = require('../constants');
 
 const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
+// const jwt = require('jsonwebtoken');
 
 const signup = (req, res) => {
     bcrypt.genSalt(10, (err, salt) => {
@@ -18,28 +18,28 @@ const signup = (req, res) => {
             req.body.password = hashedPwd;
 
             User.create(req.body)
-            .then(newUser => {
-                const token = jwt.sign(
-                    {
-                        username: newUser.username,
-                        id: newUser.id
-                    },
-                    process.env.JWT_SECRET,
-                    {
-                        expiresIn: "30 days"
-                    }
-                )
+            // .then(newUser => {
+                // const token = jwt.sign(
+                //     {
+                //         username: newUser.username,
+                //         id: newUser.id
+                //     },
+                //     process.env.JWT_SECRET,
+                //     {
+                //         expiresIn: "30 days"
+                //     }
+                // )
 
-                res.status(constants.SUCCESS).json({
-                    "token" : token,
-                    "user": newUser
-                });
-            })
-            .catch(err => {
-                res.status(constants.BAD_REQUEST).send(`ERROR: ${err}`);
-                console.log("failing here")
-                console.log(hashedPwd);
-            })
+                // res.status(constants.SUCCESS).json({
+                //     "token" : token,
+                //     "user": newUser
+                // });
+            // })
+            // .catch(err => {
+            //     res.status(constants.BAD_REQUEST).send(`ERROR: ${err}`);
+            //     console.log("failing here")
+            //     console.log(hashedPwd);
+            // })
         })
     })
 }
@@ -55,20 +55,21 @@ const login = (req, res) => {
         if(foundUser){
             bcrypt.compare(req.body.password, foundUser.password, (err, match) => {
                 if(match){
-                    const token = jwt.sign(
-                        {
-                            username: foundUser.username,
-                            id: foundUser.id
-                        },
-                        process.env.JWT_SECRET,
-                        {
-                            expiresIn: "30 days"
-                        }
-                    )
-                    res.status(constants.SUCCESS).json({
-                        "token" : token,
-                        "user": foundUser
-                    });
+                    // const token = jwt.sign(
+                    //     {
+                    //         username: foundUser.username,
+                    //         id: foundUser.id
+                    //     },
+                    //     process.env.JWT_SECRET,
+                    //     {
+                    //         expiresIn: "30 days"
+                    //     }
+                    // )
+                    res.status(constants.SUCCESS).json
+                    // ({
+                    //     "token" : token,
+                    //     "user": foundUser
+                    // });
                 } else {
                     res.status(constants.BAD_REQUEST).send(`ERROR: Incorrect Username/Password`);
                 }
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const jwt = require('jsonwebtoken');
+// const jwt = require('jsonwebtoken');
 const cors = require('cors');
 
 const app = express();
@@ -20,24 +20,24 @@ const corsOptions = {
 app.use(cors(corsOptions))
 app.use(bodyParser.json());
 
-const verifyToken = (req, res, next) => {
-    let token = req.headers['authorization'];
-    if(token){
-        token = token.substring(constants.BEARER_START_INDEX) //remove string Bearer from the token
-    }
+// const verifyToken = (req, res, next) => {
+//     let token = req.headers['authorization'];
+//     if(token){
+//         token = token.substring(constants.BEARER_START_INDEX) 
+//     }
 
-    jwt.verify(token, process.env.JWT_SECRET, (err, decodedUser) => {
-        if(err || !decodedUser){
-            return res.status(constants.UNAUTHORIZED).send(`ERROR: ${err}`);
-        }
-        req.user = decodedUser;//set the decoded payload to req object as the user information(username, id)
+//     jwt.verify(token, process.env.JWT_SECRET, (err, decodedUser) => {
+//         if(err || !decodedUser){
+//             return res.status(constants.UNAUTHORIZED).send(`ERROR: ${err}`);
+//         }
+//         req.user = decodedUser;
 
-        next();// for control to go to the next line of code
-    })
-}
+//         next();
+//     })
+// }
 
 app.use('/auth', routes.auth);
-app.use('/auth/verify', routes.auth);
+// app.use('/auth/verify', routes.auth);
 app.use('/user', routes.user);
 // app.use('/post', verifyToken, routes.post);
 
